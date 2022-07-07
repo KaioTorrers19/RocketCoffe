fd21de67704ac130f4d08b65d5cd9ab9cb86aa42
Kaio Torres <57267578+KaioTorrers19@users.noreply.github.com>
2022-07-07 17:14:05 -0300

    Add files via upload

---
+++ Rocktseat/JavaScript/script.js
@@ -0,0 +1,7 @@
+var mobile = document.getElementById("mobile-element")
+var desaparecer = document.getElementById("Hamburguer-click")
+var nav_mobile = document.getElementById("menu-mobile")
+
+desaparecer.addEventListener("click", function(){
+    desaparecer.style.display = "none"
+})
