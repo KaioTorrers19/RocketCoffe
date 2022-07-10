ce365f200601731e53bf3ee3f5ceb167b28c6e6e
Kaio Torres <57267578+KaioTorrers19@users.noreply.github.com>
2022-07-10 01:39:45 -0300

    Delete Rocktseat directory

--- Rocktseat/JavaScript/script.js
+++
@@ -1,7 +0,0 @@
-var mobile = document.getElementById("mobile-element")
-var desaparecer = document.getElementById("Hamburguer-click")
-var nav_mobile = document.getElementById("menu-mobile")
-
-desaparecer.addEventListener("click", function(){
-    desaparecer.style.display = "none"
-})
